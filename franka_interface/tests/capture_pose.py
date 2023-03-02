fixed rotation prob - use rotation matrix

franka_interface/tests/capture_pose.py
@@ -16,6 +16,7 @@ from scipy.spatial.transform import Rotation as R
 
     WARNING: The robot will move slightly (small arc swinging motion side-to-side) till code is killed.
 """
+
 def pq2tfmat(pos, quat):
     """
     pos and quat to transformation matrix
@@ -44,6 +45,7 @@ def pq2tfmat(pos, quat):
     T_obj[:3,3] = pos
     return T_obj
 
+
 def tfmat2pq(matrix):
     r = R.from_matrix(matrix[:3,:3])
     # quat = r.as_quat()
@@ -51,128 +53,174 @@ def tfmat2pq(matrix):
     pos = matrix[:3,3]
     return pos, quat
 
-def transform(pos, quat, flag=1):
+def transform(pos, quat, rotmat, flag=1):
     """
-    convert position of fl pose frame to the pose of the gel
+    convert position of ef pose frame to the pose of the gel
     or convert position of real ee pose to the fake ee pose for the cartesian command 
+    ##########
+    flange quat is bullshit, let's use ori_mat in endpoint_pose()
+    rotation2quat or quat2rotation are also messing the rotation, let's not use them
+    ###########
 
     :param pos / quat: fake end-effector pose or desired end effector pose 
     :type pos: [float] or np.ndarray
     :type quat: quaternion.quaternion or [float] (quaternion in w,x,y,z order)
+    :type rotmat: np.ndarray((3,3)) rotation matrix
     
     ef: fake end effector, 
     ee_touch: pose of DenseTact (touch)
     ee_rgb: pose of rgb camera (rgb)
-    :param flag: if flag = 1, transform ee to ef
-                 if flag = 2, transform fl to ee_touch
-                 if flag = 3, transform fl to ee_rgb
-                 if flag = 4, transform fl to ee_depth
+
+    :param flag: if flag = 1, transform ee to ef (for giving commands)
+                 if flag = 2, transform ef to ee_touch
+                 if flag = 3, transform ef to ee_rgb
+                 if flag = 4, transform ef to ee_depth
                  
     
-    return: pos, quat of the true end effector 
+    return: pos, quat, tfmat of the true end effector 
     """
 
-    T_now = pq2tfmat(pos, quat)
-    
+    T_pq = pq2tfmat(pos, quat)
+    T_now = np.eye(4)
+    T_now[:3,:3] = rotmat
+    T_now[:3,3]  = pos
+
     ee_rot2 = 0.707099974155426
     ee_z = 0.10339999943971634
-    T_fl2ef = np.eye(4)
-    # T_fl2ef[:3,:3] = np.array([[ee_rot2, -ee_rot2, 0],
-    #                         [ee_rot2, ee_rot2, 0],
-    #                         [0,0,1]])
-    # T_fl2ef[:3,3] = np.array([0,0,ee_z])
 
-    T_fl2ef[:3,:3] = np.array([[1,0, 0],
-                            [0, ee_rot2, ee_rot2],
-                            [0,-ee_rot2,ee_rot2]])
-    T_fl2ef[:3,3] = np.array([0,0,ee_z])
+    # T_fl2ef = np.eye(4)
+    # # T_fl2ef[:3,:3] = np.array([[ee_rot2, -ee_rot2, 0],
+    # #                         [ee_rot2, ee_rot2, 0],
+    # #                         [0,0,1]])
+    # # T_fl2ef[:3,3] = np.array([0,0,ee_z])
+
+    # T_fl2ef[:3,:3] = np.array([[1,0, 0],
+    #                         [0, ee_rot2, ee_rot2],
+    #                         [0,-ee_rot2,ee_rot2]])
+    # T_fl2ef[:3,3] = np.array([0,0,ee_z])
     
-    r = R.from_matrix(T_fl2ef[:3,:3])
+    # r = R.from_matrix(T_fl2ef[:3,:3])
+
+
     # flange (link 8 ) is located in 4.5mm inside of the bolted part 
     fl_distance =0.004212608
-    touch_distance = 48.57/1000
+    touch_distance = (48.57+1.7)/1000
     lenspos = 2 # 2mm from outer glass
     rgb_distance_z = (46 - lenspos)/1000
-    rgb_distance_y = 96.85/1000
+    rgb_distance_x = 96.85/1000
     depth_distance_z = 30.85/1000
-    depth_distance_y = 50/1000
-    depth_distance_x = 17.5/1000
+    depth_distance_x = 50/1000
+    depth_distance_y = -17.5/1000
 
-    touch_fin = fl_distance + touch_distance
+    touch_finz = fl_distance + touch_distance -ee_z
+    depth_finz = depth_distance_z + fl_distance - ee_z
+    rgb_finz = rgb_distance_z + fl_distance - ee_z
 
     # fl to ee 
-    sq2 = np.sqrt(2)/2
-    T_fl2ee = np.eye(4)
+    # sq2 = np.sqrt(2)/2
+    # T_fl2ee = np.eye(4)
 
-    T_fl2ee[:3,:3] = np.array([[1,0, 0],
-                            [0, np.sqrt(2)/2, np.sqrt(2)/2],
-                            [0,-np.sqrt(2)/2,np.sqrt(2)/2]])
-    T_fl2ee[:3,3] = np.array([0,touch_fin*sq2,touch_fin*sq2])
+    # T_fl2ee[:3,:3] = np.array([[1,0, 0],
+    #                         [0, np.sqrt(2)/2, np.sqrt(2)/2],
+    #                         [0,-np.sqrt(2)/2,np.sqrt(2)/2]])
+    # T_fl2ee[:3,3] = np.array([0,touch_fin*sq2,touch_fin*sq2])
+    # T_ee = np.eye(4)
+
+    # ef to ee
+    T_ef2ee = np.eye(4)
+
+
     T_ee = np.eye(4)
 
     if flag == 1:
         # convert from ee to ef for checking (don't use this for autonomous case - trajectory needed)
         # the input will be position of 
         T_w2ee = T_now
-        T_ee = np.dot(np.dot(T_w2ee, np.linalg.inv(T_fl2ee)), T_fl2ef)
+        # for flag 1, use the edge of the densetact 1 (add 25.5mm)
+        T_ef2ee[:3,3] = np.array([0,0,touch_finz+25.5/1000])
+        T_ee = np.dot(T_w2ee, np.linalg.inv(T_ef2ee))
 
     if flag == 2:
-        # fl to ee_touch 
-        T_w2ff = T_now
+        # ef to ee_touch 
+        T_w2ef = T_now
 
-        T_fl2ee[:3,3] = np.array([0,touch_fin*sq2,touch_fin*sq2])
+        T_ef2ee[:3,3] = np.array([0,0,touch_finz])
 
-        # T_ee = np.dot(T_w2ef, np.dot(np.linalg.inv(T_fl2ef), T_fl2ee))
-        T_ee = np.dot(T_w2ff, T_fl2ee)
+        T_ee = np.dot(T_w2ef, T_ef2ee)
 
     if flag == 3:
-        # fl to ee_rgb
-        T_w2ff = T_now
-        T_fl2ee[:3,3] = np.array([-rgb_distance_y,0,fl_distance + rgb_distance_z])
-        T_ee = np.dot(T_w2ff, T_fl2ee)
+        # ef to ee_rgb 
+        T_w2ef = T_now
+
+        T_ef2ee[:3,3] = np.array([rgb_distance_x,0,rgb_finz])
+
+        T_ee = np.dot(T_w2ef, T_ef2ee)
 
     if flag == 4:
-        # fl to ee_depth 
-        T_w2ff = T_now
-        T_fl2ee[:3,3] = np.array([-depth_distance_y,depth_distance_x,fl_distance + depth_distance_z])
-        T_ee = np.dot(T_w2ff, T_fl2ee)
+        # ef to ee_depth 
+        T_w2ef = T_now
+
+        T_ef2ee[:3,3] = np.array([depth_distance_x, depth_distance_y, depth_finz])
+
+        T_ee = np.dot(T_w2ef, T_ef2ee)
 
     pos1 , quat1 = tfmat2pq(T_ee)
-    Tf = quaternion_to_matrix_transform(np.array([1,0,0,0]), quat)
-    IPython.embed()
 
-    return pos1, quat1
+    # IPython.embed()
+
+    return pos1, quat1, T_ee
 
 
 def quaternion_to_matrix(q):
     # Convert a quaternion to a 3x3 rotation matrix
-    qw, qx, qy, qz = q
-    return np.array([[1 - 2*qy**2 - 2*qz**2, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw],
-                     [2*qx*qy + 2*qz*qw, 1 - 2*qx**2 - 2*qz**2, 2*qy*qz - 2*qx*qw],
-                     [2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1 - 2*qx**2 - 2*qy**2]])
+    # return np.array([[1 - 2*q.y**2 - 2*q.z**2, 2*q.x*q.y - 2*q.z*q.w, 2*q.x*q.z + 2*q.y*q.w],
+    #                  [2*q.x*q.y + 2*q.z*q.w, 1 - 2*q.x**2 - 2*q.z**2, 2*q.y*q.z - 2*q.x*q.w],
+    #                  [2*q.x*q.z - 2*q.y*q.w, 2*q.y*q.z + 2*q.x*q.w, 1 - 2*q.x**2 - 2*q.y**2]])
+    arquat = quaternion.as_float_array(q)
+    r = R.from_quat(arquat)
+    return r.as_matrix()
 
 def quaternion_inverse(q):
     # Compute the inverse of a quaternion
-    qw, qx, qy, qz = q
-    return np.array([-qx, -qy, -qz, qw]) / np.linalg.norm(q)
+    return quaternion.as_quat_array(np.array([-q.x, -q.y, -q.z, q.w]) / np.linalg.norm(quaternion.as_float_array(q)))
 
-def quaternion_to_matrix_transform(q1, q2):
-    # Compute the transformation matrix that maps a vector from the q1 coordinate system to the q2 coordinate system
-    q1_inv = quaternion_inverse(q1)
-    T = quaternion_to_matrix(q2) @ quaternion_to_matrix(q1_inv)
-    return np.vstack([np.hstack([T, np.zeros((3, 1))]), np.array([0, 0, 0, 1])])
+def quat_mult(q1, q0):
+
+    return quaternion.as_quat_array(np.array([-q1.x * q0.x - q1.y * q0.y - q1.z * q0.z + q1.w * q0.w,
+                     q1.x * q0.w + q1.y * q0.z - q1.z * q0.y + q1.w * q0.x,
+                     -q1.x * q0.z + q1.y * q0.w + q1.z * q0.x + q1.w * q0.y,
+                     q1.x * q0.y - q1.y * q0.x + q1.z * q0.w + q1.w * q0.z], dtype=np.float64))
+
+# def pq2tfmat(pos, q2):
+#     """
+#     making right pq 2 matrix matching
+#     """
+
+#     q1 = np.quaternion(1,0,0,0)
+#     # Compute the transformation matrix that maps a vector from the q1 coordinate system to the q2 coordinate system
+#     q1_inv = quaternion_inverse(q1)
+#     T = quaternion_to_matrix(q2) @ quaternion_to_matrix(q1_inv)
+#     # T_result = np.vstack([np.hstack([quaternion_to_matrix(qq), np.zeros((3, 1))]), np.array([0, 0, 0, 1])])
+#     T_result = np.vstack([np.hstack([T, np.zeros((3, 1))]), np.array([0, 0, 0, 1])])
+#     T_result[:3,3] = pos
+#     return T_result
 
 def test_pose():
 
-    pos_f, quat_f = r.get_flange_pose()
-
-    pos_touch, quat_touch = transform(pos_f, quat_f, 2)
-    # pos_rgb, quat_rgb = transform(pos_f, quat_f, 3)
-    # pos_depth, quat_depth = transform(pos_f, quat_f, 4)
-    print("flange: ", pos_f, quat_f)
+    pos = r.endpoint_pose()['position']
+    quat = r.endpoint_pose()['orientation']
+    rotmat = r.endpoint_pose()['ori_mat']
+    
+    pos_touch, quat_touch, tf_touch = transform(pos, quat, rotmat, 2)
+    pos_rgb, quat_rgb, tf_rgb  = transform(pos, quat, rotmat, 3)
+    pos_depth, quat_depth, tf_depth  = transform(pos, quat, rotmat, 4)
+    print("ef pose: ", r.endpoint_pose())
     print("touch: ", pos_touch, quat_touch)
-    # print("rgb: ", pos_rgb, quat_rgb)
-    # print("depth: ", pos_depth, quat_depth)
+    print(tf_touch)
+    print("rgb: ", pos_rgb, quat_rgb)
+    print(tf_rgb)
+    print("depth: ", pos_depth, quat_depth)
+    print(tf_depth)
 
 if __name__ == '__main__':
     # pos = np.array([0,0,0])